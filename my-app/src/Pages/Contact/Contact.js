3cf129c60bfbca131d7b9544ae33a7e93bbaefa9
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-14 21:30:31 +0330

    modified contact component

--- my-app/src/Pages/Contact/Contact.js
+++ my-app/src/Pages/Contact/Contact.js
@@ -1,9 +1,19 @@
-import './Contact.css';
+
+
+import { FaFacebookF, FaInstagram,FaYoutube } from 'react-icons/fa6';import './Contact.css';
+
 const Contact = () => {
     return ( 
         <div className='cantact-me'>
             <p>Let Me Give You A Beautifull Website</p>
             <a className='button-two' href="#">Hire Me</a>
+            <p></p>
+            <div className='social'>
+               <a className='i'><FaInstagram/></a> 
+                <a className='i'><FaYoutube/></a>
+                <a className='i'><FaFacebookF/></a>
+            </div>
+            <p className='end'>CopyRight By Mehraneh</p>
         </div>
      );
 }
