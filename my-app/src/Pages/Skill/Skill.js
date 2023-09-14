2d2c458fb50d3f68f69074a47acd447b3314df61
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-14 15:51:58 +0330

    Update Skill.js

--- my-app/src/Pages/Skill/Skill.js
+++ my-app/src/Pages/Skill/Skill.js
@@ -1,8 +1,6 @@
-import './Skill.css'
-
-const Skill = () => {
+const Skill = ({name,describtion,image}) => {
     return ( 
-<section style={{display:'flex', flexDirection:'column' , width:'80%', margin:'auto' , justifyContent:'center',alignItem:'start'}}>
+<div style={{display:'flex', flexDirection:'column' , width:'80%', margin:'auto' , justifyContent:'center',alignItem:'start'}}>
 <header className='skill'>My Skills</header>
 <div style={{width:'50px',innerHeight:'5px', backGround:'red'}}></div>
 <div className='container' style={{display:'flex', flexDirection:'row',gap:"50px" , width:'80%', margin:'auto' , justifyContent:'center',alignItem:'center'}}>
@@ -10,56 +8,15 @@ const Skill = () => {
 <div className='skillBox' style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItem:'center'}}>
     <div className='skilltitle'>
     <div className='skillImage'>
-        <div><img className='img' src='./images/js.png'/></div>
-       <div><h2>JAVA SCRIPT</h2></div>
-       <div><p>Lore lus vitae congue mauris rhoncus aeneat, consectetur adipiscing elit, sed do eiusmon vel elit scelerisque.   </p></div>
+        <div><img className='img' src={image}/></div>
+       <div><h2>{name}</h2></div>
+       <div><p>{describtion}</p></div>
     </div>
     </div>
 </div>
-<div className='skillBox' style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItem:'center'}}>
-    <div >
-    <div className='skilltitle' >
-        <div><img className='img' src='./images/html.png'/></div>
-       <div><h2>HTML 5</h2></div>
-       <div><p style={{alignItem:'left'}}>Lore lus vitae congue mauris rhoncus aeneat, consectetur adipiscing elit, sed do eiusmon vel elit scelerisque.   </p></div>
-    </div>
-    </div>
 </div>
-<div className='skillBox' style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItem:'center'}} >
-    <div className='skilltitle'>
-    <div className='skillImage'>
-        <div><img className='img' src='./images/tw.png'/></div>
-       <div><h2>TAILWIND</h2></div>
-       <div><p>Lore lus vitae congue mauris rhoncus aeneat, consectetur adipiscing elit, sed do eiusmon vel elit scelerisque.   </p></div>
-    </div>
-    </div>
 </div>
-
-<div className='skillBox' style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItem:'center'}}>
-    <div className='skilltitle'>
-    <div className='skillImage'>
-        <div><img className='img' src='./images/bt.png'/></div>
-       <div><h2>BOOTSTRAPT</h2></div>
-       <div><p>Lore lus vitae congue mauris rhoncus aeneat, consectetur adipiscing elit, sed do eiusmon vel elit scelerisque.   </p></div>
-    </div>
-    </div>
-</div>
-<div className='skillBox' style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItem:'center'}}>
-    <div className='skilltitle'>
-    <div className='skillImage'>
-        <div><img className='img' src='./images/react.png'/></div>
-       <div><h2>REACT</h2></div>
-       <div><p>Lore lus vitae congue mauris rhoncus aeneat, consectetur adipiscing elit, sed do eiusmon vel elit scelerisque.   </p></div>
-    </div>
-    </div>
-</div>
-
-</div>
-
-</section>
-
      );
 }
  
 export default Skill;
-
