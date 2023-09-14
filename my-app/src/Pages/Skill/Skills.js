cc38cb74c4195ccd5325bbbf8e247f5768210608
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-14 17:15:39 +0330

    edit skills app

--- my-app/src/Pages/Skill/Skills.js
+++ my-app/src/Pages/Skill/Skills.js
@@ -1,20 +1,27 @@
 import './Skill.css'
-import html from '.images/html.png';
-import css from '.images/css.png';
-import js from '.images/js.png';
-import bt from '.images/bt.png';
-import tw from '.images/tw.png';
-import react from '.images/react.png';
-import Skill from './skill';
+import Skill from './Skill';
+import html from '/image/html.png';
+import css from '/image/css.png';
+import js from '/image/js.png';
+import bt from '/image/bt.png';
+import tw from '/image/tw.png';
+import react from '/image/react.png';
+
 const Skills = () => {
     return ( 
-<div>
-<Skill Skillname='HTML' describtion='' image={html}/>
+<div style={{display:'flex', flexDirection:'column' , width:'80%', margin:'auto' , justifyContent:'center',alignItem:'start'}}>
+<header className='skill'>My Skills</header>
+<div style={{width:'50px',innerHeight:'5px', backGround:'red'}}></div>
+<div className='container' style={{display:'grid',gridTemplateColumns:' auto auto  auto',gap:"200px" ,padding:'20px', width:'100%', margin:'auto' , justifyContent:'center',alignItem:'center'}}>
+
+<Skill  name='HTML' describtion='' image={html}/>
 <Skill name='CSS' describtion='' image={css}/>
 <Skill name='JAVASCRIPT' describtion='' image={js}/>
 <Skill name='REACT' describtion='' image={bt}/>
 <Skill name='TAILWIND' describtion='' image={tw}/>
-<Skill name='BOOTSTRAP' describtion='' image={react}/>
+<Skill name='REACT' describtion='' image={react}/>
+
+</div>
 </div>
 
 
