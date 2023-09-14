4f894c4910bd290f4d94a1e42da765636368be44
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-14 21:30:07 +0330

    modified services component

--- my-app/src/Pages/Services/Services.js
+++ my-app/src/Pages/Services/Services.js
@@ -1,6 +1,58 @@
+import './Services.css';
+import { FaRegUser,FaBars,FaRegBell } from 'react-icons/fa6';
 const Services = () => {
     return (  
-        <div className="services"></div>
+        <div className="services">
+            <div className='title'>
+                <h2>SERVICES</h2>
+            </div>
+            <div className='box'>
+
+
+
+
+                <div className='card'>
+           <FaBars className='i'/>
+                    <h5>WEB DEVELOPER</h5>
+                    <div className='pra'>
+                        <p>every website should be built two primary goals:firstly: it needs to work across all devices.Secondly, it needs to be fast as possible  </p>
+                    
+                    <p style ={{textAlign:'center'}} ></p>
+                     <a className='button' href='#' >READ MORE</a>
+                     </div>
+                </div>
+          
+
+
+
+          
+                <div className='card'>
+               <FaRegUser className='i'/>
+                    <h5>WEB DEVELOPER</h5>
+                    <div className='pra'>
+                        <p>every website should be built two primary goals:firstly: it needs to work across all devices.Secondly, it needs to be fast as possible  </p>
+                    
+                    <p style ={{textAlign:'center'}} ></p>
+                     <a className='button' href='#' >READ MORE</a>
+                     </div>
+                </div>
+      
+
+           
+                <div className='card'>
+               <FaRegBell className='i'/>
+                    <h5>WEB DEVELOPER</h5>
+                    <div className='pra'>
+                        <p>every website should be built two primary goals:firstly: it needs to work across all devices.Secondly, it needs to be fast as possible  </p>
+                    
+                    <p style ={{textAlign:'center'}} ></p>
+                     <a className='button' href='#' >READ MORE</a>
+                     </div>
+                </div>
+            </div>
+            </div>
+      
+   
     );
 }
  
