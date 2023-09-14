ddf8e6af8f99a533f0789065eb05a0a438177e40
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-14 17:15:21 +0330

    edit skill app

--- my-app/src/Pages/Skill/Skill.js
+++ my-app/src/Pages/Skill/Skill.js
@@ -1,9 +1,5 @@
 const Skill = ({name,describtion,image}) => {
     return ( 
-<div style={{display:'flex', flexDirection:'column' , width:'80%', margin:'auto' , justifyContent:'center',alignItem:'start'}}>
-<header className='skill'>My Skills</header>
-<div style={{width:'50px',innerHeight:'5px', backGround:'red'}}></div>
-<div className='container' style={{display:'flex', flexDirection:'row',gap:"50px" , width:'80%', margin:'auto' , justifyContent:'center',alignItem:'center'}}>
 
 <div className='skillBox' style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItem:'center'}}>
     <div className='skilltitle'>
@@ -14,8 +10,7 @@ const Skill = ({name,describtion,image}) => {
     </div>
     </div>
 </div>
-</div>
-</div>
+
      );
 }
  
